5b53d66b6e852dbd1847fc03e67a58ab8bab7ead
ghk829 <ghk829@hanmail.net>
2018-10-30 03:19:28 +0900

    fuckfuck

--- app.js
+++ app.js
@@ -8,7 +8,52 @@ var express = require('express')
   , path = require('path');
 
   var app = express();
+  var winston = require('winston');
+  const logConfig = {
+  levels:{
+  emerg:0,  
+  alert:1,
+  crit:2,
+  error:3,
+  warning:4,
+  notice:5,
+  info:6,
+  debug:7	
+  }
+  ,colors:{
+  emerg:'red',  
+  alert:'red',
+  crit:'red',
+  error:'yellow',
+  warning:'yellow',
+  notice:'yellow',
+  info:'blue',
+  debug:'blue'
+  }}
 
+  var logger = winston.createLogger({
+    levels: winston.config.syslog.levels,
+    // 아래에서 설명할 여러개의 transport를 추가할 수 있다.
+    transports: [
+        // Console transport 추가
+  	  
+        new (winston.transports.Console)({
+
+          format : winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.align()
+          )
+
+        }),
+       
+        // File transport 추가
+       /*new (winston.transports.File)({
+           // filename property 지정
+           filename: 'somefile.log'
+       })*/
+    ]
+  });
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('rootDir',__dirname)
@@ -27,53 +72,6 @@ var message = require('@route/message')(app)
 var keyboard = require('@route/keyboard')(app)
 app.use('/keyboard',keyboard)
 app.use('/message',message);
-
-var winston = require('winston');
-const logConfig = {
-levels:{
-emerg:0,  
-alert:1,
-crit:2,
-error:3,
-warning:4,
-notice:5,
-info:6,
-debug:7	
-}
-,colors:{
-emerg:'red',  
-alert:'red',
-crit:'red',
-error:'yellow',
-warning:'yellow',
-notice:'yellow',
-info:'blue',
-debug:'blue'
-}}
-
-var logger = winston.createLogger({
-  levels: winston.config.syslog.levels,
-  // 아래에서 설명할 여러개의 transport를 추가할 수 있다.
-  transports: [
-      // Console transport 추가
-	  
-      new (winston.transports.Console)({
-
-        format : winston.format.combine(
-          winston.format.colorize(),
-          winston.format.timestamp(),
-          winston.format.align()
-        )
-
-      }),
-     
-      // File transport 추가
-     /*new (winston.transports.File)({
-         // filename property 지정
-         filename: 'somefile.log'
-     })*/
-  ]
-});
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   logger.info('Express server listening on port ' + app.get('port'));
--- controller/keyboard.js
+++ controller/keyboard.js
@@ -1,5 +1,4 @@
 exports.keyboard = function(req, res){
-	logger.info(req);
 	var result = {"type":"text"};
 	res.json(result);	
 };
